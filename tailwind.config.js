2f4d530219d7f2c27dc8f76dae1036fb6654d6c6
Mutesi <ktesialine@gmail.com>
2022-10-21 08:11:34 +0200

    desktop design

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,14 +8,31 @@ module.exports = {
         "gradient-color-two": "#ff333d",
         "gradient-color-three": "#90e0ff",
         "gradient-color-four": "#ffcb57",
+        "dark-blue": "#0a2540",
+        "light-blue": "#635bff",
+        "light-gray": "#f6f9fc",
+        primary: "#0a2540",
+      },
+      padding: {
+        hundered: "100px",
+      },
+      margin: {
+        hundered: "100px",
+      },
+      fontSize: {
+        medium: "15px",
       },
       width: {
         fit: "fit-content",
         "18r": "18rem",
+        "medium-width": "480px"
+      },
+      fontSize: {
+        eighty: "80px",
       },
       height: {
-        fit: "fit-content"
-      }
+        fit: "fit-content",
+      },
     },
     plugins: [],
   },
